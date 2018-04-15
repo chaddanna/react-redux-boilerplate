2d48922ff82eb4f0ff2fef5bdc0d0a7b9b4b8d20
Chad Danna <chad@formulated.io>
2018-04-15 09:37:00 -0600

    Format andremove absolute paths

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Store from './store';
 import App from './app';
-import 'src/scss/index.css';
+import './scss/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Store>
-    <App />
-  </Store>,
-  document.getElementById('root')
+	<Store>
+		<App />
+	</Store>,
+	document.getElementById('root')
 );
 registerServiceWorker();

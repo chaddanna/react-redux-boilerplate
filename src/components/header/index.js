88ae37c2b144fbbeaae860f0c860b404ffca98fb
Chad Danna <chad@formulated.io>
2018-04-15 09:34:22 -0600

    Remove useless styling and remove logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import logo from 'src/images/logo.svg';
 import './index.css';
 
 class Header extends Component {
-  render() {
-    return (
-      <header className="header">
-        <img src={logo} className="logo" alt="logo" />
-        <h1 className="title">{this.props.title}</h1>
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header className="header">
+				<span className="title">{this.props.title}</span>
+			</header>
+		);
+	}
 }
 
 Header.propTypes = {
-  title: PropTypes.string
+	title: PropTypes.string
 };
 
 export default Header;
